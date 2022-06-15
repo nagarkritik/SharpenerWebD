6e71cff0ef248f97af7a1549862b7eea8b1ebe17
Kritik Nagar <nagarkritik@gmail.com>
2022-06-15 22:37:26 +0530

    task 6

--- dom/index.js
+++ dom/index.js
@@ -13,7 +13,7 @@ addItems.style.color = "green"
 
 var listItems = document.getElementsByClassName("list-group-item")
 
-listItems[2].style.backgroundColor = "green"
+//listItems[2].style.backgroundColor = "green"
 
 for(var i = 0; i<listItems.length;i++){
     listItems[i].style.fontWeight = "bold"
@@ -21,3 +21,23 @@ for(var i = 0; i<listItems.length;i++){
 
 document.getElementsByClassName("list-group").items.children[4].textContent = "red"
 document.getElementsByTagName("li")[4].style.color = "red"
+
+//var item2 = document.querySelector("#items").children[1]
+//item2.style.backgroundColor = "green"
+
+//var item3 = document.querySelector("#items").children[2]
+//item3.style.visibility = "hidden"
+
+//var item2 = document.querySelectorAll(".list-group-item")[1]
+//item2.style.backgroundColor = "green"
+
+var listItems = document.querySelectorAll(".list-group-item")
+
+for(var i=0; i<listItems.length;i++){
+    if(i%2!=0){
+        listItems[i].style.backgroundColor = "green"
+    }
+}
+
+// querySelector selects only first element matching to the query passsed inside it.
+// whereas querySelectorAll gives a list of all the matching elements

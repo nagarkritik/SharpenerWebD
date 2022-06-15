8bf572bb4288e2dae2a5663e3f2a4b4e83d6d3f5
Kritik Nagar <nagarkritik@gmail.com>
2022-06-15 21:59:29 +0530

    Task3 - Lets get started with dom

--- dom/index.js
+++ dom/index.js
@@ -1,3 +1,16 @@
+
+//Innertext pays attention to sttlying whereas text content does not pay 
+// attention to styling. textContent just gives all the text present inside
+// the selected element irrespective of styling.
+
+var header = document.getElementById("main-header")
+header.style.border = "solid 3px #000"
+
+var addItems = document.getElementsByClassName("title")[0]
+
+addItems.style.fontWeight = "bold"
+addItems.style.color = "green"
+
 var listItems = document.getElementsByClassName("list-group-item")
 
 listItems[2].style.backgroundColor = "green"

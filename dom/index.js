3a1af4308c146a025c5b73f5e9a62b110e098f4f
Kritik Nagar <nagarkritik@gmail.com>
2022-06-16 11:39:56 +0530

    task 7

--- dom/index.js
+++ dom/index.js
@@ -1,4 +1,4 @@
-
+/*
 //Innertext pays attention to sttlying whereas text content does not pay 
 // attention to styling. textContent just gives all the text present inside
 // the selected element irrespective of styling.
@@ -41,3 +41,81 @@ for(var i=0; i<listItems.length;i++){
 
 // querySelector selects only first element matching to the query passsed inside it.
 // whereas querySelectorAll gives a list of all the matching elements
+
+*/
+
+// Traversing The DOM 
+
+// Using parentNode property
+ var items = document.querySelector("#items")
+// items.parentNode.style.backgroundColor = "rgb(205, 208, 208)"
+// console.log(items.parentNode.parentNode)
+
+// var items = document.querySelector("#items")
+// items.parentElement.style.backgroundColor = "rgb(205, 208, 208)"
+// console.log(items.parentElement.parentElement)
+
+// Using .childNode property
+//console.log(items.childNodes) // childNodes also gives <br> as childs
+
+// .children
+console.log(items.children)
+items.children[1].style.backgroundColor = "yellow"
+
+// .firstChild : considers <br> as childs
+console.log(items.firstChild)
+
+// .firstElementChild
+console.log(items.firstElementChild)
+items.firstElementChild.textContent = "Hello1"
+
+// .lastChild : considers <br> as children
+console.log(items.lastChild)
+
+// .lastElementChild
+console.log(items.lastElementChild)
+items.lastElementChild.textContent = "Hello5"
+
+// .nextSibling
+console.log(items.nextSibling)
+
+// .nextElementSibling
+console.log(items.nextElementSibling)
+
+// .previousSibling
+console.log(items.previousSibling)
+
+// .previosElementSibling
+console.log(items.previousElementSibling)
+items.previousElementSibling.style.color = "green"
+
+// createElement
+
+// create a div
+var newdiv = document.createElement("div")
+// Add id 
+newdiv.id = "hello1"
+// Add class
+newdiv.className = "hello"
+// Add attr
+newdiv.setAttribute("title", "Hello div")
+
+// Adding some text inside div
+// creating text node
+ var newDivText = document.createTextNode("Hello World")
+
+// Adding this text node to div
+newdiv.appendChild(newDivText)
+
+// Adding this div to dom
+
+var container = document.querySelector("header .container")
+var h1 = document.querySelector("header h1")
+
+container.insertBefore(newdiv, h1)
+
+var ul = document.querySelector("#items")
+var item1 = document.querySelector("#items").firstElementChild
+
+ul.insertBefore(newdiv,item1)
+console.log(newdiv)

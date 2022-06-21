ef75aa2e44708c2693cd6c1ace96ab104c5c30bd
Kritik Nagar <nagarkritik@gmail.com>
2022-06-21 14:14:38 +0530

    Making registration page dynamic Task 5 part 2

--- dom/practice.js
+++ dom/practice.js
@@ -284,10 +284,11 @@ function createPost(post){
         setTimeout(()=>{
             posts.push(post)
             console.log(post.title+" created")
+
             const error = false
 
             if(!error){
-                resolve()
+                resolve(post)
             }else{
                 reject("Error: Something went wrong")
             }
@@ -310,10 +311,10 @@ function getPost(){
 
 
 
-
 function deletePost(){
     let p = new Promise((resolve, reject)=>{
         let error = false
+        console.log("delete calles")
 
         if(posts.length===0){
             error = true
@@ -335,10 +336,28 @@ function deletePost(){
 
 }
 
-createPost({title:"Post3", body:"This is post3"}).then(deletePost).catch(err=>console.log(err))
+createPost({title:"Post3", body:"This is post3"})
+.then(getPost).catch(err=>console.log(err))
+.then(deletePost)
+.then(getPost).catch(err=>console.log(err))
+Promise.all
 
+// const p1 = Promise.resolve("Hello World")
+// const p2 = 10
+// const p3 = new Promise((resolve, reject)=>{
+//     setTimeout(resolve, 2000, "Goodbye")
+// })
+// const p4 = fetch("")
+// Promise.all([p1,p2,p3]).then(values=>console.log(values))
 
+function updateLastUserActivityTime(){
+    return new Promise((resolve, reject)=>{
+        setTimeout(resolve, 1000, new Date())
+    })
+}
+const p4 = createPost({title:"Post3", body:"This is post3"})
+const p5 = updateLastUserActivityTime()
 
+Promise.all([p4,p5]).then(values=>console.log(values)).then(deletePost).then(getPost)
 
-
-
+//createPost({title:"Post3", body:"This is post3"}).then(getPost).catch(err=>console.log(err))

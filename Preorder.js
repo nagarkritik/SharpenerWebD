0de8cb3edbefa26f9eaf122106cfc3478cc376db
Kritik Nagar <nagarkritik@gmail.com>
2022-06-22 19:18:24 +0530

    Preorder Traversal Task 15

---
+++ Preorder.js
@@ -0,0 +1,46 @@
+class Node{
+    constructor(val){
+        this.val = val
+        this.left = null
+        this.right = null
+    }
+}
+
+function preorderRec(root){
+    if(root){
+        pre.push(root.val)
+        preorderRec(root.left)
+        preorderRec(root.right)
+    }
+}
+
+function preorderIt(root){
+    if(!root) return
+
+    let res = []
+    let st = []
+    st.push(root)
+
+    while(st.length!=0){
+        let a = st.pop()
+        res.push(a.val)
+
+        if(a.right) st.push(a.right)
+        if(a.left) st.push(a.left)
+    }
+
+    return res
+}
+
+let pre = []
+let root = new Node(1)
+root.left = new Node(2)
+root.right = new Node(3)
+root.left.left = new Node(4)
+root.left.right = new Node(5)
+root.right.left = new Node(6)
+root.right.right = new Node(7)
+
+preorderRec(root)
+console.log(pre)
+console.log(preorderIt(root))

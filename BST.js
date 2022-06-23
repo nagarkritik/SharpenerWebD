6d84e4b02b73c9baa3f7b9a25a4a5ee7f8eb7104
Kritik Nagar <nagarkritik@gmail.com>
2022-06-23 21:17:13 +0530

    Min and Max in BST

--- BST.js
+++ BST.js
@@ -48,16 +48,51 @@ class BST{
         else if(value<curr.val) return this.searchHelp(value, curr.left)
         else return this.searchHelp(value, curr.right)
     }
+
+    findMin(){
+        if(!this.root) return false
+
+
+        let min = this.findMinHelper(this.root)
+        return min.val
+    }
+
+    findMinHelper(curr){
+        while(curr.left){
+            curr = curr.left
+        }
+        return curr
+    }   
+
+    findMax(){
+        if(!this.root) return false
+
+        let max = this.findMaxHelper(this.root)
+        return max.val
+    }
+
+    findMaxHelper(curr){
+        while (curr.right){
+            curr = curr.right
+        }
+        return curr
+    }
 }
 
 
 let t1 = new BST()
 
-t1.insert(10)
-t1.insert(9)
 t1.insert(8)
-t1.insert(11)
-t1.insert(12)
+t1.insert(3)
+t1.insert(10)
+t1.insert(1)
+t1.insert(6)
+t1.insert(14)
+t1.insert(4)
+t1.insert(7)
 t1.insert(13)
 
+
 console.log(t1.search(9))
+console.log(t1.findMin())
+console.log(t1.findMax())

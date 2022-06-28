103f73572562052c8f25ff4968076596f127f424
Kritik Nagar <nagarkritik@gmail.com>
2022-06-28 20:21:12 +0530

    Intro to middlewares - task 2

--- Express/app.js
+++ Express/app.js
@@ -1,22 +1,29 @@
 const express = require('express')
-
-// const http = require('http')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-// Middleware 1
-app.use((req,res,next)=>{
-    console.log("In the middleware")
-    next() // Allows the req to move to next middleware in line
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/favicon.ico',(req, res, next)=>{
+    console.log("I don't know why this fevicon request runs")
+})
+app.use('/add-product', (req, res, next)=>{
+    res.send(`<form action="/product" method="POST">
+    <input type="text" name="title">
+    <input type="text" name="size">
+    <button type="submit">Add Product</button></form>`)
 })
 
-// Middleware2
-app.use((req,res,next)=>{
-    console.log("In another middleware")
+app.post('/product',(req, res, next)=>{
+    console.log(req.body)
+    console.log(req.body.title)
+    console.log(req.body.size)
+    res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
     res.send("<h1>Hello from express</h1>") // allows us to send response of any type
 })
 
-// const server = http.createServer(app)
-// server.listen(3000)
 
 app.listen(4000)

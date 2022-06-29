dfa1609cbe6ce6123f0b032bed9d7b04d4249e99
Kritik Nagar <nagarkritik@gmail.com>
2022-06-29 23:03:18 +0530

    What are models - task 8

--- Express/models/product.js
+++ Express/models/product.js
@@ -1,48 +1,36 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
+const rootDir = require('../util/path')
+const p = path.join(rootDir,'data', 'products.json')
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  'data',
-  'products.json'
-);
+const getProductsFromFile = (cb)=>{
 
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([]);
-    } else {
-      cb(JSON.parse(fileContent));
+    fs.readFile(p, (err, fileContent)=>{
+        if(err){
+            cb([])
+        }else{ cb(JSON.parse(fileContent))}
+    } )
+}
+
+module.exports = class Product{
+    constructor(title){
+        this.title = title
     }
-  });
-};
 
-module.exports = class Product {
-  constructor(title, imageUrl, description, price) {
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+    save(){
+        getProductsFromFile(products=>{
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err)=>{
+                console.log(err)
+            })
+        })
 
-  save() {
-    this.id = Math.random().toString();
-    getProductsFromFile(products => {
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);
-      });
-    });
-  }
+        // readind the data to of file to get the array of products already stored in data file
+        
+    }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb);
-  }
+    static fetchAll(cb){
+        getProductsFromFile(cb)
+    }
 
-  static findById(id, cb) {
-    getProductsFromFile(products => {
-      const product = products.find(p => p.id === id);
-      cb(product);
-    });
-  }
-};
+}

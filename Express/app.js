cff926a89349d4925cdb03d270271cf08750a411
Kritik Nagar <nagarkritik@gmail.com>
2022-07-01 23:42:49 +0530

    Design the user and Product relation task 18

--- Express/app.js
+++ Express/app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const  sequelize = require('./util/database')
+const Product = require('./models/product')
+const User = require('./models/user')
+
 const errorController = require('./controllers/error');
 
 const app = express();
@@ -25,15 +28,39 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res, next)=>{
+    User.findByPk(1)
+    .then(user=>{
+        req.user = user
+        next()
+    })
+    .catch(err=>console.log(err))
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, {constraints: true.valueOf, onDelete: 'CASCADE'})
+User.hasMany(Product)
+
 sequelize.sync()
 .then(res=>{
     //console.log(res)
+    User.findByPk(1)
+}).then(user=>{
+    if(!user){
+        return User.create({
+            name: "Max",
+            email: "test@test.com"
+        })
+        return user
+    }
+}).then(user=>{
+    console.log(user)
     app.listen(3000);
-}).catch(err=> console.log(err))
+})
+.catch(err=> console.log(err))
 
 
--- Express/controllers/admin.js
+++ Express/controllers/admin.js
@@ -13,12 +13,12 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  
-  Product.create({
+  req.user.createProduct(   {
     title: title,
     price: price,
     imageUrl: imageUrl,
-    description: description
+    description: description,
+
   }).then(result=>{
     console.log("Created Product")
     res.redirect('/admin/products')
@@ -33,8 +33,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(product=>{
+  req.user.getProducts({where:{id: prodId}})
+  .then(products=>{
+    const product = products[0]
     if(!product){
       return res.redirect('/')
     }
@@ -72,7 +73,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  // Product.findAll()
+  req.user.getProducts()
   .then(products=>{
     res.render('admin/products', {
       prods: products,

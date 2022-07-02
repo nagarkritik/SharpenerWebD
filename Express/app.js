e9f040c7302602bb59fb37f51cba6fafb26e5b78
Kritik Nagar <nagarkritik@gmail.com>
2022-07-02 12:42:51 +0530

    Cart with many to many relation - task19

--- Express/app.js
+++ Express/app.js
@@ -3,11 +3,12 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const  sequelize = require('./util/database')
-const Product = require('./models/product')
-const User = require('./models/user')
-
 const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -17,50 +18,50 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-// db.execute('select*from products')
-// .then((res)=>{
-//     console.log(res[0])
-// })
-// .catch((err)=>{
-//     console.log(err)
-// })
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req,res, next)=>{
-    User.findByPk(1)
-    .then(user=>{
-        req.user = user
-        next()
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      req.user = user;
+      next();
     })
-    .catch(err=>console.log(err))
-})
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-Product.belongsTo(User, {constraints: true.valueOf, onDelete: 'CASCADE'})
-User.hasMany(Product)
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
-sequelize.sync()
-.then(res=>{
-    //console.log(res)
-    User.findByPk(1)
-}).then(user=>{
-    if(!user){
-        return User.create({
-            name: "Max",
-            email: "test@test.com"
-        })
-        return user
+sequelize
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
     }
-}).then(user=>{
-    console.log(user)
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
-})
-.catch(err=> console.log(err))
-
-
+  })
+  .catch(err => {
+    console.log(err);
+  });
